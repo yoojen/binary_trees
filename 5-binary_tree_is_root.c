4692fbc2542632e7ace2b1980f3f7b0ca32d0153
Eugene MUTUYIMANA <eugenemutuyimana15@gmail.com>
2023-08-01 12:08:45 -0700

    check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,7 +9,7 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node || node->parent != NULL)
-        return 0;
-    return 1;
+	if (!node || node->parent != NULL)
+		return (0);
+	return (1);
 }
