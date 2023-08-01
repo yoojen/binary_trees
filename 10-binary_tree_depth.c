b3706bce297194783d49d9cac35142b51f4da9ea
Yoseph Tamirat <yosephtamir@gmail.com>
2023-08-01 22:26:41 +0300

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - is used to find the debth of the binary tree
+ * @tree: is the tree to be measured
+ * Return: depth or 0 if error occurs
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree || !tree->parent)
+		return (0);
+
+	int i = binary_tree_depth(tree->parent);
+
+	return (i + 1);
+}
