c6da95a27713641c01132437d37076682264107b
Yoseph Tamirat <yosephtamir@gmail.com>
2023-08-03 22:18:56 +0300

    sibling update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,9 +9,6 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node->parent)
-		return (NULL);
-
 	binary_tree_t *par = node->parent;
 
 	if (node == NULL || par == NULL || !par->left || !par->right)
